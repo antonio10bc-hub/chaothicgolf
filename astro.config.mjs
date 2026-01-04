d52268c6fa5d92150ec945ad513f90a0e01a77e3
antonio10bc-hub <antonioasis10bc@gmail.com>
2026-01-04 18:31:38 +0100

    dependencies vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,18 @@
-// astro.config.mjs
 import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
-import react from '@astrojs/react';
+import tailwind from "@astrojs/tailwind";
+import react from "@astrojs/react";
 
+// https://astro.build/config
 export default defineConfig({
-  integrations: [
-    tailwind(),
-    react()
-  ]
+  integrations: [tailwind(), react()],
+  vite: {
+    ssr: {
+      // ESTO ES LA MAGIA: Obligamos a procesar estas librerías
+      noExternal: ['@tsparticles/react', '@tsparticles/slim'],
+    },
+    // Añadimos esto por si acaso para mejorar la compatibilidad
+    optimizeDeps: {
+      include: ['@tsparticles/react', '@tsparticles/slim']
+    }
+  }
 });
